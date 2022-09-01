e70d5ef25499d54602b6a52924f9a68c1414dedb
ajitrchv <67496650+ajitrchv@users.noreply.github.com>
2022-09-01 22:36:57 +0530

    part 1 of building cars app using all fuctions yet lrearned

---
+++ cars/urls.py
@@ -0,0 +1,12 @@
+from importlib.resources import path
+from django.urls import path
+from . import views
+
+
+app_name = 'cars'
+
+urlpatterns = [
+    path('list/',views.list,name='list'),
+    path('add/',views.add,name='add'),
+    path('delete/',views.delete,name='delete'),
+]
